fd33201fc25ebd8a402f53cf02c28b4cdc86ce55
DeveloperSamvel <developer.vardanyan@gmail.com>
2023-11-13 01:18:23 +0400

    changes getDog function and html file

--- developer/dogs/index.js
+++ developer/dogs/index.js
@@ -1,5 +1,16 @@
-function getDog() {
-    const apiUrl = 'https://dog.ceo/api/breed/akita/images/random';
+function getDog(elementsBox) {
+    let breed = "";
+    if (elementsBox[elementsBox.selectedIndex].value) {
+        breed = elementsBox[elementsBox.selectedIndex].value;
+    }
+
+    if (breed === "") {
+        document.querySelector("#imgResult").querySelector("img").setAttribute('src', "");
+
+        return;
+    }
+
+    const apiUrl = `https://dog.ceo/api/breed/${breed}/images/random`;
     const options = {
         method: 'GET',
         headers: {
@@ -16,48 +27,53 @@ function getDog() {
             return response.json()
         })
         .then(data => {
-            console.log("Полученные данные:", data);
+            document.querySelector("#imgResult").querySelector("img").setAttribute('src', data.message);
         })
         .catch(error => {
             console.error('Ошибка запроса:', error);
         });
 }
 
-getDog();
+function getListAllBreeds() {
+    const apiUrl = 'https://dog.ceo/api/breeds/list/all';
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/plain;charset=UTF-8'
+        }
+    };
 
-// function getListAllBreeds() {
-//     console.log("test");
-//     const apiUrl = 'https://dog.ceo/api/breeds/list/all';
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//     'Content-Type': 'text/plain;charset=UTF-8'
-            // }
-//     };
+    return fetch(apiUrl, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.json());
+            }
 
-//     return fetch(apiUrl, options)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error(response.json());
-//             }
+            return response.json()
+        })
+        .then(data => {
+            if(data.message) {
+                let arrOption = Object.keys(data.message);
+                let newOption = document.createElement("option");
+                const newContent = document.createTextNode("-- Select a breed --");
+                newOption.setAttribute("value", '');
+                newOption.appendChild(newContent);
+                document.querySelector("#listAllBreeds").appendChild(newOption);
+    
+                arrOption.forEach((option) => {
+                    let newOptionInForEach = document.createElement("option")
+                    const newContentInForEach = document.createTextNode(option);
+                    newOptionInForEach.setAttribute("value", option);
+                    newOptionInForEach.appendChild(newContentInForEach);
+                    document.querySelector("#listAllBreeds").appendChild(newOptionInForEach);
+                });
 
-//             return response.json()
-//         })
-//         .then(data => {
-//             // data
-//             // document.getElementById("listAllBreeds") 
-//             // let option = document.createElement("option");
-//             // console.log("option", option);
-//             // document.getElementById("listAllBreeds").appendChild(option);
-//             console.log("option", data.message);
-//             console.log("option", Array.from(data.message));
-//             return Array.from(data.message);
-//         })
-//         .catch(error => {
-//             console.error('Ошибка запроса:', error);
-//         });
-// }
+                console.log("option typeof", Object.keys(data.message));
+            }
+        })
+        .catch(error => {
+            console.error('Ошибка запроса:', error);
+        });
+}
 
-// getListAllBreeds();
-// let listAllBreeds = getListAllBreeds();
-// console.log("listAllBreeds", listAllBreeds);
+getListAllBreeds();
