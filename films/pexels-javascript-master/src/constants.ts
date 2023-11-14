ed3101a1a8f1fdd489cb6e7e7a1836d4eb890055
Karo Kendimian <chesmorana@gmail.com>
2023-11-14 18:27:37 +0400

    half way thru: fetch and search done

--- films/pexels-javascript-master/src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const photoBaseUrl = "https://api.pexels.com/v1/";
-export const videoBaseUrl = "https://api.pexels.com/videos/";
-export const collectionBaseUrl = "https://api.pexels.com/v1/collections/";
--- films/script.js
+++ films/script.js
@@ -1,11 +1,45 @@
-import { createClient } from "pexels";
+let queryText = "landscape";
+const searchForm = document.getElementById("searchForm");
+const photosGrid = document.querySelector("#photosGrid");
 
-const client = createClient(
-  "TrAOaNFEuh7CtaF1xbqatu4L7Je9VPjuwlGWdEZ9oVbfDaoCpyldLRZd"
-);
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let inputText = document.getElementById("searchInput").value;
 
-client.photos.show({ id: 2014422 }).then((photo) => {});
+  if (inputText != "") {
+    queryText = inputText;
+    photosGrid.innerHTML = "";
 
-async function getCuratedPhotos() {
-  const curatedPhotos = await fetch("");
+    fetch(`https://api.pexels.com/v1/search?query=${queryText}`, {
+      headers: {
+        Authorization:
+          "TrAOaNFEuh7CtaF1xbqatu4L7Je9VPjuwlGWdEZ9oVbfDaoCpyldLRZd",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        handleImages(data.photos);
+      });
+  }
+});
+
+function handleImages(photos) {
+  photos.map((photo) => {
+    const photoCard = `<div title="${photo.alt}" class="photoCard"><a href="${photo.src.original}"><img src="${photo.src.tiny}" /></a></div>`;
+    photosGrid.innerHTML += photoCard;
+  });
 }
+
+fetch(`https://api.pexels.com/v1/curated?page=2&per_page=40`, {
+  headers: {
+    Authorization: "TrAOaNFEuh7CtaF1xbqatu4L7Je9VPjuwlGWdEZ9oVbfDaoCpyldLRZd",
+  },
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    handleImages(data.photos);
+  });
